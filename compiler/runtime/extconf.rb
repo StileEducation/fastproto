51c4511a50ae117b5f011b54f13828fcdcf46759
deoxxa <deoxxa@fknsrs.biz>
2017-04-26 21:49:20 +1000

    use have_library to link protobuf in runtime code

--- compiler/runtime/extconf.rb
+++ compiler/runtime/extconf.rb
@@ -1,15 +1,24 @@
 require 'mkmf'
-dir_config('protobuf')
-dir_config('boost')
+
+$INCFLAGS << ' -I/usr/local/include'
+$LIBPATH.push('/usr/local/lib')
 $CXXFLAGS << ' --std=c++14 -Wno-shorten-64-to-32 -O3'
-$LOCAL_LIBS << ' -lprotobuf'
+
+dir_config('protobuf')
+
+unless have_library('protobuf')
+    abort "protobuf is missing. please install protobuf"
+end
+
 # All of our objects are in subdirectories (that match the original proto subdirectories)
 # but mkmf only compiles stuff in the toplevel. Add targets to $objs representing *every*
 # cpp/cc file in the tree.
 $objs =  Dir['**/*.{cpp,cc,c}'].map { |f|
     File.join(File.dirname(f), File.basename(f, '.*')) + '.' + CONFIG['OBJEXT']
 }
+
 create_makefile('fastproto_gen')
+
 # We now need to hack at the generated makefile to add a cleanup job (using a bash extglob)
 # to clean up the extra targets we added.
 makefile_text = File.read('Makefile')
@@ -17,4 +26,5 @@ makefile_text.gsub!(/^\\s*SHELL\\s*=.*$/, 'SHELL=/bin/bash -O extglob -c')
 makefile_text += <<-EOS
     CLEANOBJS:= $(CLEANOBJS) **/*.#{CONFIG['OBJEXT']}
 EOS
+
 File.open('Makefile', 'w') { |f| f.write makefile_text }
